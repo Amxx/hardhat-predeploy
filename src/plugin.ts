08c4e22c4c1d1e3ec217a6fa08774290a92f4ad5
Hadrien Croubois <hadrien.croubois@gmail.com>
2025-07-18 22:46:26 +0200

    fix coverage compatibility by hooking into tasks

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,17 +1,39 @@
+import { extendConfig, extendEnvironment, task } from "hardhat/config";
+import type { EIP1193Provider, HardhatRuntimeEnvironment } from "hardhat/types";
 import {
-  extendConfig,
-  extendEnvironment,
-  extendProvider,
-  task,
-} from "hardhat/config";
-import type { EIP1193Provider } from "hardhat/types";
-import {
+  TASK_NODE_SERVER_READY,
   TASK_RUN,
   TASK_TEST_SETUP_TEST_ENVIRONMENT,
 } from "hardhat/builtin-tasks/task-names";
 
 import { resolveWithDefault } from "./config";
 import { set } from "./types";
+
+extendConfig((config, { predeploy: userConfig }) => {
+  config.predeploy = resolveWithDefault(userConfig);
+});
+
+extendEnvironment((hre) => {
+  hre.predeploy = {};
+});
+
+/// NOTE: this approach doesn't work in coverage mode.
+// extendProvider(async (provider, config /*network*/) => {
+//   const isDev = await checkIfDevelopmentNetwork(provider);
+//   if (isDev) {
+//     await Promise.all(
+//       Object.entries(config.predeploy)
+//         .filter(([, details]) => details)
+//         .map(([address, { bytecode }]) =>
+//           provider.request({
+//             method: "hardhat_setCode",
+//             params: [address, bytecode],
+//           }),
+//         ),
+//     );
+//   }
+//   return provider;
+// });
 
 async function checkIfDevelopmentNetwork(
   provider: EIP1193Provider,
@@ -27,61 +49,33 @@ async function checkIfDevelopmentNetwork(
   );
 }
 
-extendConfig((config, { predeploy: userConfig }) => {
-  config.predeploy = resolveWithDefault(userConfig);
-});
-
-extendEnvironment((hre) => {
-  hre.predeploy = {};
-});
-
-extendProvider(async (provider, config /*network*/) => {
-  const isDev = await checkIfDevelopmentNetwork(provider);
+async function deployAndSetEnv(env: HardhatRuntimeEnvironment) {
+  const isDev = await checkIfDevelopmentNetwork(env.network.provider);
   if (isDev) {
     await Promise.all(
-      Object.entries(config.predeploy)
+      Object.entries(env.config.predeploy)
         .filter(([, details]) => details)
-        .map(([address, { bytecode }]) =>
-          provider.request({
-            method: "hardhat_setCode",
-            params: [address, bytecode],
-          }),
+        .map(([address, { name, abi, bytecode }]) =>
+          env.network.provider
+            .request({
+              method: "hardhat_setCode",
+              params: [address, bytecode],
+            })
+            .then(() => env.ethers.getContractAt(abi, address))
+            .then((instance) => set(env.predeploy, name, instance)),
         ),
     );
   }
-  return provider;
-});
+}
+
+task(TASK_NODE_SERVER_READY).setAction((_, env, runSuper) =>
+  runSuper().then(() => deployAndSetEnv(env)),
+);
 
 task(TASK_RUN).setAction((_, env, runSuper) =>
-  runSuper().then(async () => {
-    const isDev = await checkIfDevelopmentNetwork(env.network.provider);
-    if (isDev) {
-      await Promise.all(
-        Object.entries(env.config.predeploy)
-          .filter(([, details]) => details)
-          .map(([address, { name, abi }]) =>
-            env.ethers
-              .getContractAt(abi, address)
-              .then((instance) => set(env.predeploy, name, instance)),
-          ),
-      );
-    }
-  }),
+  runSuper().then(() => deployAndSetEnv(env)),
 );
 
 task(TASK_TEST_SETUP_TEST_ENVIRONMENT).setAction((_, env, runSuper) =>
-  runSuper().then(async () => {
-    const isDev = await checkIfDevelopmentNetwork(env.network.provider);
-    if (isDev) {
-      await Promise.all(
-        Object.entries(env.config.predeploy)
-          .filter(([, details]) => details)
-          .map(([address, { name, abi }]) =>
-            env.ethers
-              .getContractAt(abi, address)
-              .then((instance) => set(env.predeploy, name, instance)),
-          ),
-      );
-    }
-  }),
+  runSuper().then(() => deployAndSetEnv(env)),
 );
