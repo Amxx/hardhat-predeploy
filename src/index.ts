d3220ef6ab348b73d6f35b4103c2e9c24f0c9f1c
Hadrien Croubois <hadrien.croubois@gmail.com>
2025-09-03 15:55:50 +0200

    refactor sub plugins

--- src/index.ts
+++ src/index.ts
@@ -2,40 +2,35 @@ import type { HardhatPlugin } from "hardhat/types/plugins";
 
 import "./type-extensions";
 
-const hardhatPredeployCore: HardhatPlugin = {
-  id: "hardhat-predeploy-core",
+const hardhatPredeployPlugin: HardhatPlugin = {
+  id: "hardhat-predeploy",
   hookHandlers: {
     config: () => import("./hook-handlers/config.js"),
     network: () => import("./hook-handlers/network-deploy.js"),
   },
-};
-
-const hardhatPredeployEthers: HardhatPlugin = {
-  id: "hardhat-predeploy-ethers",
-  hookHandlers: {
-    network: () => import("./hook-handlers/network-populate-ethers.js"),
-  },
-  dependencies: () => [Promise.resolve({ default: hardhatPredeployCore }), import("@nomicfoundation/hardhat-ethers")],
-};
-
-const hardhatPredeployViem: HardhatPlugin = {
-  id: "hardhat-predeploy-viem",
-  hookHandlers: {
-    network: () => import("./hook-handlers/network-populate-viem.js"),
-  },
-  dependencies: () => [Promise.resolve({ default: hardhatPredeployCore }), import("@nomicfoundation/hardhat-viem")],
-};
-
-const hardhatPredeployPlugin: HardhatPlugin = {
-  id: "hardhat-predeploy",
   dependencies: () => [
-    Promise.resolve({ default: hardhatPredeployCore }),
     import("@nomicfoundation/hardhat-viem").then(
-      () => ({ default: hardhatPredeployViem }),
+      hardhatViem => ({
+        default: {
+          id: "hardhat-predeploy-viem",
+          hookHandlers: {
+            network: () => import("./hook-handlers/network-populate-viem.js"),
+          },
+          dependencies: () => [Promise.resolve(hardhatViem)],
+        },
+      }),
       () => ({ default: { id: "@nomicfoundation/hardhat-viem/not-found" } }),
     ),
     import("@nomicfoundation/hardhat-ethers").then(
-      () => ({ default: hardhatPredeployEthers }),
+      hardhatEthers => ({
+        default: {
+          id: "hardhat-predeploy-ethers",
+          hookHandlers: {
+            network: () => import("./hook-handlers/network-populate-ethers.js"),
+          },
+          dependencies: () => [Promise.resolve(hardhatEthers)],
+        },
+      }),
       () => ({ default: { id: "@nomicfoundation/hardhat-ethers/not-found" } }),
     ),
   ],
